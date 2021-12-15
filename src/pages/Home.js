2b5db0dc820c8148e710c4b0c3a462c0566b925a
ShivKumar <75515360+shivkumarprajapat@users.noreply.github.com>
2021-12-15 22:06:35 +0530

    loop User Array Data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,36 @@
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import Loader from '../components/Loader';
+import User from '../components/User';
 import { getUsers } from '../store/action/userAction';
 
 export default function Home() {
-    const dispatch = useDispatch();
+    const { users, loader } = useSelector((state) => state.users);
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(getUsers());
+	}, []);
 
-    useEffect(() => {
-        dispatch(getUsers())
-    })
-
-    return (
-        <h1>List of users</h1>
-    )
+    return !loader ?
+        <div className='container mt-5 shadow rounded-lg'>
+            <table className="table table-responsive-md ">
+            <thead>
+                <tr>
+                    <th scope="col">Sr No</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Username</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Details</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                   users.map((user, index) => (
+                    <User key={index} index={index} user={user} />
+                    ))
+                }
+            </tbody>
+        </table>
+        </div>
+        : <Loader/>
 }
